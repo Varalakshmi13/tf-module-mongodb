eeba2b0970a44f118c9b50e1f98b73d8fb9c1de1
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-28 06:54:33 +0530

    Updating tf-module-mongodb

--- cluster.tf
+++ cluster.tf
@@ -1,11 +1,12 @@
-//resource "aws_docdb_cluster" "docdb" {
-//  cluster_identifier      = "roboshop-${var.ENV}"
-//  engine                  = "docdb"
-//  master_username         = "admin1"
-//  master_password         = "roboshop1"
-//  #this is not for prod
-//  skip_final_snapshot     = true
-//}
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "roboshop-${var.ENV}"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+  #this is not for prod
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+}
 
 
 resource "aws_docdb_subnet_group" "docdb" {
