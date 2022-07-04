e8ea1d80728c435cd10a882fe3fbda8734c31d90
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-07-04 20:44:28 +0530

    Updating tf-module-mongodb

--- cluster.tf
+++ cluster.tf
@@ -6,6 +6,7 @@ resource "aws_docdb_cluster" "docdb" {
   #this is not for prod
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+  vpc_security_group_ids = [aws_security_group.allow_mongodb.id]
 }
 
 
