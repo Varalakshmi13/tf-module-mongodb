001447376321c9d5945cbf4bd59f4c39590bb71c
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-29 07:52:18 +0530

    Updating tf-module-mongodb

--- schema.tf
+++ schema.tf
@@ -6,8 +6,8 @@ cd /tmp
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 unzip -o mongodb.zip
 cd mongodb-main
-mongo --tls --tlsCAFile /home/centos/rds-combined-ca-bundle.pem --host ${local.DNS_NAME} --username ${local.USERNAME} --password ${local.PASSWORD} < catalogue.js
-mongo --tls --tlsCAFile /home/centos/rds-combined-ca-bundle.pem --host ${local.DNS_NAME} --username admin1 --password ${local.PASSWORD} < users.js
+mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${local.DNS_NAME} --username ${local.USERNAME} --password ${local.PASSWORD} < catalogue.js
+mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${local.DNS_NAME} --username ${local.USERNAME} --password ${local.PASSWORD}  < users.js
 EOF
   }
 }
